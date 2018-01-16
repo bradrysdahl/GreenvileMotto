c0515d9838ab702fdb1432b1c54cd968a367aef9
bradrysdahl <35493803+bradrysdahl@users.noreply.github.com>
2018-01-16 08:33:00 -0600

    Update Program.cs

--- GreenvileMotto/GreenvilleMotto2/GreenvilleMotto2/Program.cs
+++ GreenvileMotto/GreenvilleMotto2/GreenvilleMotto2/Program.cs
@@ -1,4 +1,9 @@
-﻿using static System.Console;
+﻿//Program written by: Brad Rysdahl
+//Program date: 1/16/18
+//Program description: This program displays the Greenville Motto
+//surrounded by a border of asterisks.
+
+using static System.Console;
 namespace GreenvilleMotto2
 {
     class Program
